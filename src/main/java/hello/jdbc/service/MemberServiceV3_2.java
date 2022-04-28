39bb196394f1555c4202d8ba652b4418ad28e172
choiyoonho <cyh7834@gmail.com>
2022-04-28 22:01:03 +0900

    트랜잭션 문제 해결 - 트랜잭션 AOP 이해

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,63 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ * */
+@Slf4j
+public class MemberServiceV3_2 {
+    private final TransactionTemplate transactionTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+
+    public MemberServiceV3_2(MemberRepositoryV3 memberRepository, PlatformTransactionManager transactionTemplate) {
+        this.transactionTemplate = new TransactionTemplate(transactionTemplate);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        transactionTemplate.executeWithoutResult((status) -> {
+            try {
+                transfer(fromId, toId, money);
+            } catch (SQLException throwables) {
+                throw new IllegalStateException(throwables);
+            }
+        });
+    }
+
+    private void transfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex"))
+            throw new IllegalStateException("이체중 예외 발생");
+    }
+
+    private void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 커넥션 풀 고려
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+}
