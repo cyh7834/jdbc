011ca08e463af8b6442f634e6bfbeaee6b7183f5
choiyoonho <cyh7834@gmail.com>
2022-04-30 17:45:34 +0900

    트랜잭션 문제 해결

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -2,38 +2,25 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionTemplate;
-
-import java.sql.Connection;
+import org.springframework.transaction.annotation.Transactional;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 템플릿
+ * 트랜잭션 - @Transactional AOP
  * */
 @Slf4j
-public class MemberServiceV3_2 {
-    private final TransactionTemplate transactionTemplate;
+public class MemberServiceV3_3 {
     private final MemberRepositoryV3 memberRepository;
 
 
-    public MemberServiceV3_2(MemberRepositoryV3 memberRepository, PlatformTransactionManager transactionTemplate) {
-        this.transactionTemplate = new TransactionTemplate(transactionTemplate);
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        transactionTemplate.executeWithoutResult((status) -> {
-            try {
-                transfer(fromId, toId, money);
-            } catch (SQLException throwables) {
-                throw new IllegalStateException(throwables);
-            }
-        });
+        transfer(fromId, toId, money);
     }
 
     private void transfer(String fromId, String toId, int money) throws SQLException {
